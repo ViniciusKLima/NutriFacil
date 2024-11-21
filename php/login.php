f27564f1cceaef9415cd3d3bdc2d2025caaed2d7
ViniciusKLima <viniciuskls2006@gmail.com>
2024-11-21 16:11:05 -0300

    replit att

--- php/login.php
+++ php/login.php
@@ -15,7 +15,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['username'] = $username;
         header("Location: ../opcoes-NF/opcoes.php");
     } else {
-        echo "Usuário ou senha incorretos";
+        echo '<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Incorreto</title>
+</head>
+<body>
+<style>
+    body{
+        margin: 0;
+        padding: 0;
+        display: flex;
+        overflow: hidden;
+        font-family: arial;
+        background: linear-gradient(to right, #3C593D, #438E46, #9FD873);
+    }
+
+    div{
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        color: white;
+    }
+
+    h1{
+        max-width: 60%;
+    }
+
+    a{
+        color: inherit;
+    }
+
+</style>
+
+    <div>
+        <h1>Usuário ou senha incorretos! :(</h1>
+        <a href="../login/login.html">Faça login novamente.</a>
+    </div>
+</body>
+</html>';
+        exit;
     }
 }
 ?>
--- php/register.php
+++ php/register.php
@@ -8,9 +8,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
         $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
         $stmt->execute([$username, $password]);
-        header("Location: ../login/login2.php");
+        header("Location: ../login/login.html");
     } catch (Exception $e) {
-        echo "Erro ao cadastrar: " . $e->getMessage();
+        echo '<!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Login Incorreto</title>
+        </head>
+        <body>
+        <style>
+            body{
+                margin: 0;
+                padding: 0;
+                display: flex;
+                overflow: hidden;
+                font-family: arial;
+                background: linear-gradient(to right, #3C593D, #438E46, #9FD873);
+            }
+
+            div{
+                width: 100vw;
+                height: 100vh;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                color: white;
+            }
+
+            h1{
+                max-width: 60%;
+            }
+
+            a{
+                color: inherit;
+            }
+
+        </style>
+
+            <div>
+                <h1>Este nome de usuário já existe</h1>
+                <a href="../login/login.html">Cadastre novamente com outro nome.</a>
+            </div>
+        </body>
+        </html>';
+                exit;
     }
 }
 ?>
